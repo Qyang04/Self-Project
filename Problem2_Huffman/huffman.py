f0661c627a4a88f107656be09d110d44fb958c89
jordancmh <jordanchaymh@gmail.com>
2025-07-26 23:57:50 +0800

    Replaced use of count() and sort() built-in functions with manual implementations

--- Problem2_Huffman/huffman.py
+++ Problem2_Huffman/huffman.py
@@ -1,28 +1,4 @@
-# import heapq
-# from collections import defaultdict
-
-# def build_huffman_tree(text):
-#     freq = defaultdict(int)
-#     for char in text:
-#         freq[char] += 1
-#     heap = [[weight, [char, ""]] for char, weight in freq.items()]
-#     heapq.heapify(heap)
-#     while len(heap) > 1:
-#         lo = heapq.heappop(heap)
-#         hi = heapq.heappop(heap)
-#         for pair in lo[1:]:
-#             pair[1] = '0' + pair[1]
-#         for pair in hi[1:]:
-#             pair[1] = '1' + pair[1]
-#         heapq.heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
-#     return heap[0][1:]
-
-# if __name__ == "__main__":
-#     text = input("Enter text to encode: ")
-#     huffman_codes = build_huffman_tree(text)
-#     print("Huffman Codes:")
-#     for char, code in sorted(huffman_codes, key=lambda x: len(x[1])):
-#         print(f"'{char}': {code}")
+#TODO: Implement visualization of Huffman tree
 
 # Node class; also stores character and its frequency
 class Node:
@@ -32,22 +8,30 @@ class Node:
         self.left = None
         self.right = None
 
-nodes = []  # List of unique character Nodes
+nodes = []  # Stores nodes for building Huffman tree
 
 # Count occurences of characters in word, and append to nodes list
 def calculate_frequencies(word):
     frequencies = {}
     for char in word:
-        if char not in frequencies:
-            freq = word.count(char)
-            frequencies[char] = freq
-            nodes.append(Node(char, freq))
+        if char in frequencies:
+            frequencies[char] += 1
+        else:
+            frequencies[char] = 1
+    for unique_char in frequencies:
+        nodes.append(Node(unique_char, frequencies[char]))
+        
+# Manual impl of bubble sort that sorts Nodes based on freq value
+def bubble_sort_nodes(list):
+    n = len(list)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if list[j].freq > list[j + 1].freq:
+                list[j], list[j + 1] = list[j + 1], list[j]
 
 def build_huffman_tree():
     while len(nodes) > 1:
-        nodes.sort(
-            key=lambda x: x.freq  # Sort all nodes based on freq, in ascending order
-        )
+        bubble_sort_nodes(nodes)  # Sort all nodes based on freq, in ascending order
         left = nodes.pop(0)
         right = nodes.pop(0)
 
